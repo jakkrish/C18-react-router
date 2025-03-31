3078e979ecfda152295dca2ffc2135fd85c5ba76
jakkrish <jak.krish@gmail.com>
2025-03-31 21:09:23 +0530

    react-router implemented

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,27 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, Link, Routes, Route } from 'react-router';
 import './index.css';
 import App from './App.tsx';
+
+import About from './About';
+import Products from './Products';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+    <ul>
+      <li><Link to="/"  >Home</Link></li>
+      <li><Link to="/about"  >About</Link></li>
+      <li><Link to="/products"  >Products</Link></li>
+    </ul>
+    <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+      
     </BrowserRouter>
   </StrictMode>
 );
